794ce6194b939b44d877bd444a520284ebeaa847
DBryzz <domoubrice@gmail.com>
2020-08-27 14:50:11 +0100

    redirect to blog after login

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,17 @@ use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    } 
+
     /**
      * Display a listing of the resource.
      *
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/', 'PostController@index');
 Route::get('/posts/knowUs', 'PostController@knowUs')->name('posts.knowUs');
